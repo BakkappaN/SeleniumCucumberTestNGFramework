047c8a8525a855f57ab3fd93ee8901df970c7112
BakkappaN <bakkappa123@yahoo.com>
2019-07-14 12:53:11 +0530

    Fifth commit from another branch

--- SeleniumCucumberBDDFramework/src/com/testautomation/TestScripts/CommitTest.java
+++ SeleniumCucumberBDDFramework/src/com/testautomation/TestScripts/CommitTest.java
@@ -6,6 +6,7 @@ public class CommitTest {
 		System.out.println("Testing commit");
 		System.out.println("Selenium Java TestNG Tutorial - Bakkappa N");
 		System.out.println("selenium by bakkappa n");
+		System.out.println("TestNG tutorial by bakkappa n");
 	}
 
 }
